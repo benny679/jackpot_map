d7e2021f5e94e8a5e8308c92307294ee20fb075b
benny679 <benjaminaydani17@gmail.com>
2025-03-06 18:07:43 +0000

    Update home.py

--- home.py
+++ home.py
@@ -42,6 +42,8 @@ if check_password():
     - **Dashboard**: View and filter the jackpot map data
     - **Admin Panel**: (Admin only) View login activity and manage IP settings
     - **User Management**: (Admin only) Manage user accounts and permissions
+    - **Manual Value Tracking**: Jackpots that are being tracked manually via Research Ops Daily Value Tracking
+    - **KPIs**: (Admin only) Check how poorly or how well some parts of the RNG project are doing, purely for bragging rights and shit talking
     """)
 
     # Show some quick stats
